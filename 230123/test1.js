cd8309799a62782afeba3735592fe6a12d609ebc
Wojciech Domalewski <info@pnti.pl>
2023-01-23 15:50:03 +0100

    Testy

--- 230123/test1.js
+++ 230123/test1.js
@@ -4,10 +4,13 @@ function Test(pierwszy)
     this.pierwszy = pierwszy;
 }
 
-
-
+let test = new Test('pierwszy')
 
 console.log(test)
+test.pierwszy = 'drugi'
 
+console.log(test.pierwszy)
+
+// jest dostęp do skladowych obiektu test
 
 
