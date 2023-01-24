0303a449eb289b7e1ee39cb50f0ab0d88ca96542
Wojciech Domalewski <info@pnti.pl>
2023-01-24 11:19:53 +0100

    Testing various constructors

--- 230123/test6.js
+++ 230123/test6.js
@@ -1,6 +1,14 @@
-Person.prototype.hello = () => console.log('Hello')
 
-function Person(){}
+// To działa
+// function Person(){}
+
+// To działa
+//const Person = function (){}
+
+// To nie działa
+//const Person = () => {}
+
+//Person.prototype.hello = () => console.log('Hello')
 
 let test = new Person()
 
