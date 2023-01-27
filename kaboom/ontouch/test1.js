abfef138e941dda57ec2c62deb8dc11185a1942a
Wojciech Domalewski <info@pnti.pl>
2023-01-27 16:11:13 +0100

    Obsługa dotykania konkretnego przycisku działa

--- kaboom/ontouch/test1.js
+++ kaboom/ontouch/test1.js
@@ -4,17 +4,41 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 // initialize kaboom context
 kaboom();
 
-loadSprite('ikona','ikona1.png')
+/*
+    Dotknięcie ikony ma zmieniać obrazek
+*/
 
-add([
-    sprite('ikona'),
+loadSprite('ikona2','../media/ikona2.png')
+loadSprite('ikona3','../media/ikona3.png')
+
+const ikona = add([
+    sprite('ikona2'),
     pos(120, 80),
+    area()
 ]);
 
 let x = 0
+let pierwsza = true
 
-// Dotyczy dotknięcia na 
+// Dotknięcie przycisku sprawdza się za pomocą metody hasPoint(pos)
+// https://www.youtube.com/watch?v=sB4Oq8D3p2c&t=117s
+// (UWAGA: Mysz trzeba obsłużyć oddzielnie)
 
 onTouchEnd((id, pos)=>{
-    debug.log(x++ + "pos: " + pos + "id: " + id)
+
+    if(ikona.hasPoint(pos) && pierwsza)
+
+    {
+        ikona.use(sprite('ikona3'))
+        pierwsza = false
+    }
+
+    else if(ikona.hasPoint(pos))
+
+    {
+        ikona.use(sprite('ikona2'))
+        pierwsza = true
+    }
+
+    debug.log(x++ + " pos: " + pos + " id: " + id)
 })
